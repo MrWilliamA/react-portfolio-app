9cd0263222d75e8677fd8afcefdcfb352b8a1e10
MrWilliamA <will.archer@hotmail.com>
2022-06-11 13:25:32 +1000

    minor fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import SocialIcons from "./SocialIcons";
 
 const Footer = () => {
   return (
-    <div className=" w-screen bottom-0 mt-[80px] flex items-center px-14 h-[100px] z-10 bg-darkblue place-content-between">
+    <div className="max-w-screen bottom-0 mt-[80px] flex items-center px-14 h-[100px] z-10 bg-darkblue place-content-between">
       <div>
         <p class="text-white font-bold text-sm md:text-base">
           Copyright @ 2022
